16105ed7ada0127b449463c0e141c06bc00d4faf
khushi-pes231 <162816546+khushi-pes231@users.noreply.github.com>
2024-03-10 20:53:34 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hellooooo, World\n";
-  cout << "Hello, Jenkins\n";
+  cout << "Hello, Jenkinssss\n";
   cout << "I have successfully built and run\n";
   
   return 0;
